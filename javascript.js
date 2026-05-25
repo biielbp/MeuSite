04865632cf0f40c28b6097405a56f39a15288637
biielbp <166571033+biielbp@users.noreply.github.com>
2026-05-25 13:53:28 -0300

    Refactor particle animation to typing effect

--- javascript.js
+++ javascript.js
@@ -1,59 +1,31 @@
-const canvas = document.getElementById("particles");
-const ctx = canvas.getContext("2d");
+const texto = "Hello World!";
+const elemento = document.querySelector(".texto-digitando");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+let i = 0;
+let apagando = false;
 
-let particlesArray = [];
+function efeitoDigitar() {
+    if (!apagando) {
+        // digitando
+        elemento.innerHTML = texto.substring(0, i + 1);
+        i++;
 
-class Particle {
-    constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 1;
-        this.speedX = Math.random() * 1 - 0.5;
-        this.speedY = Math.random() * 1 - 0.5;
+        if (i === texto.length) {
+            apagando = true;
+            setTimeout(efeitoDigitar, 1500); // pausa antes de apagar
+            return;
+        }
+    } else {
+        // apagando
+        elemento.innerHTML = texto.substring(0, i - 1);
+        i--;
+
+        if (i === 0) {
+            apagando = false;
+        }
     }
 
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-
-        if (this.x > canvas.width) this.x = 5;
-        if (this.x < 0) this.x = canvas.width;
-        if (this.y > canvas.height) this.y = 0;
-        if (this.y < 0) this.y = canvas.height;
-    }
-
-    draw() {
-        ctx.fillStyle = "#ffffff";
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fill();
-    }
+    setTimeout(efeitoDigitar, apagando ? 50 : 100);
 }
 
-function init() {
-    for (let i = 0; i < 250; i++) {
-        particlesArray.push(new Particle());
-    }
-}
-
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    particlesArray.forEach(p => {
-        p.update();
-        p.draw();
-    });
-
-    requestAnimationFrame(animate);
-}
-
-window.addEventListener("resize", () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-});
-
-init();
-animate();
+efeitoDigitar();
